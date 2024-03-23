fb3224d743a516fcba81ce0248d14255aae52ac0
ahmdhjj <ahmadelhajj.dev@gmail.com>
2024-03-23 12:59:40 -0400

    Add tests

---
+++ tests/settings.py
@@ -0,0 +1,56 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF = 'tests.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'intra',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['intra/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+SECRET_KEY = 'Random-Secret-Key-Intra'
+
+USE_I18N = False
+
+USE_TZ = False
+
+LOGIN_REDIRECT_URL = '/admin/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Intra settings
+
+INTRA_RESET_PASSWORD_ENABLED = True
