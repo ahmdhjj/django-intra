b8f58aa2caf0abfb1443a5db9ee579df6b75af80
ahmdhjj <ahmadelhajj.dev@gmail.com>
2024-03-23 12:59:34 -0400

    Add a quick setting to enable the password reset feature Fixes #4

--- intra/urls.py
+++ intra/urls.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-urlpatterns = [
+from . import intra_settings
+
+urlpatterns = []
+
+if intra_settings.INTRA_RESET_PASSWORD_ENABLED:
+    urlpatterns += [
+        path(
+            'password_reset/',
+            auth_views.PasswordResetView.as_view(
+                extra_context={'site_header': admin.site.site_header}
+            ),
+            name='admin_password_reset',
+        ),
+        path(
+            'password_reset/done/',
+            auth_views.PasswordResetDoneView.as_view(
+                extra_context={'site_header': admin.site.site_header}
+            ),
+            name='password_reset_done',
+        ),
+        path(
+            'reset/<uidb64>/<token>/',
+            auth_views.PasswordResetConfirmView.as_view(
+                extra_context={'site_header': admin.site.site_header}
+            ),
+            name='password_reset_confirm',
+        ),
+        path(
+            'reset/done/',
+            auth_views.PasswordResetCompleteView.as_view(
+                extra_context={'site_header': admin.site.site_header}
+            ),
+            name='password_reset_complete',
+        ),
+    ]
+
+urlpatterns += [
     path('', admin.site.urls),
 ]
